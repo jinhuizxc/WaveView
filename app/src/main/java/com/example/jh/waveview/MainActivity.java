c2ec801bc8ae2fbb5df49e4e050487915d55b6fa
jinhuizxc <1004260403@qq.com>
2018-01-22 14:57:42 +0800

    test

--- app/src/main/java/com/example/jh/waveview/MainActivity.java
+++ app/src/main/java/com/example/jh/waveview/MainActivity.java
@@ -5,6 +5,8 @@ import android.graphics.Paint;
 import android.support.v4.view.animation.LinearOutSlowInInterpolator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * 关于水波纹的制作。自定义view
@@ -12,6 +14,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     private WaveView mWaveView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mWaveView = (WaveView) findViewById(R.id.wave_view);
+        button = (Button) findViewById(R.id.search);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 设置初始半径
+//                mWaveView.setInitialRadius(100);
+                mWaveView.setDuration(5000);
+                mWaveView.setStyle(Paint.Style.FILL);
+                mWaveView.setColor(Color.RED);
+                mWaveView.setInterpolator(new LinearOutSlowInInterpolator());
+                mWaveView.start();
+
+                mWaveView.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mWaveView.stop();
+                    }
+                }, 5000);
+            }
+        });
+
+
 
 /*        mWaveView.setDuration(5000);
         mWaveView.setStyle(Paint.Style.STROKE);
@@ -27,17 +53,8 @@ public class MainActivity extends AppCompatActivity {
         mWaveView.setInterpolator(new AccelerateInterpolator(1.2f));
         mWaveView.start();*/
 
-        mWaveView.setDuration(5000);
-        mWaveView.setStyle(Paint.Style.FILL);
-        mWaveView.setColor(Color.RED);
-        mWaveView.setInterpolator(new LinearOutSlowInInterpolator());
-        mWaveView.start();
 
-//        mWaveView.postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                mWaveView.stop();
-//            }
-//        }, 10000);
+
+
     }
 }
--- app/src/main/java/com/example/jh/waveview/WaveView.java
+++ app/src/main/java/com/example/jh/waveview/WaveView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
@@ -15,9 +16,15 @@ import java.util.List;
 /**
  * 水波纹特效
  * Created by fbchen2 on 2016/5/25.
+ * <p>
+ * 关于View的onMeasure()、onSizeChanged()、onLayout()、onDraw()调用顺序
+ * <p>
+ * 继承与View和继承与现有控件都是下面的顺序，但是控件的大小是生成之后就固定的，不会再次改变。
+ * onMeasure()→onSizeChanged()→onLayout()→onMeasure()→onLayout()→onDraw()
  */
 public class WaveView extends View {
 
+    private static final String TAG = "WaveView";
     private float mInitialRadius;   // 初始波纹半径
     private float mMaxRadius;   // 最大波纹半径
     private long mDuration = 2000; // 一个波纹从创建到消失的持续时间
@@ -26,7 +33,8 @@ public class WaveView extends View {
     private boolean mMaxRadiusSet;
 
     private boolean mIsRunning;
-    private long mLastCreateTime;
+    private long mLastCreateTime;  //
+    // 链表存储产生的圆
     private List<Circle> mCircleList = new ArrayList<Circle>();
 
     private Runnable mCreateCircle = new Runnable() {
@@ -34,6 +42,9 @@ public class WaveView extends View {
         public void run() {
             if (mIsRunning) {
                 newCircle();
+                // 不加postDelayed的话只能绘制一个圆
+                // 要做的事情，这里再次调用此Runnable对象，
+                // 以实现每两秒实现一次的定时器操作
                 postDelayed(mCreateCircle, mSpeed);
             }
         }
@@ -55,10 +66,23 @@ public class WaveView extends View {
         mPaint.setStyle(style);
     }
 
+    /**
+     * onSizeChanged()方法是系统自动调用的。
+     * w，h是view当前的宽和高;oldw ,oldh是改变之前的宽和高。
+     *
+     * @param w
+     * @param h
+     * @param oldw
+     * @param oldh
+     */
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        //   w, h, oldw, oldh = 1080,1750,0,0
+        Log.e(TAG, "w, h, oldw, oldh = " + w + "," + h + "," + oldw + "," + oldh);
         if (!mMaxRadiusSet) {
             mMaxRadius = Math.min(w, h) * mMaxRadiusRate / 2.0f;
+            Log.e(TAG, "Math.min(w, h) =" + Math.min(w, h)); // 返回最小的那个数字
+            Log.e(TAG, "mMaxRadius =" + mMaxRadius);  // mMaxRadius =459.0 = 1080*0.85/2.0
         }
     }
 
@@ -76,6 +100,7 @@ public class WaveView extends View {
     public void start() {
         if (!mIsRunning) {
             mIsRunning = true;
+            // 线程启动，产生圆圈
             mCreateCircle.run();
         }
     }
@@ -101,6 +126,7 @@ public class WaveView extends View {
         while (iterator.hasNext()) {
             Circle circle = iterator.next();
             float radius = circle.getCurrentRadius();
+            // 当前时间 - 创建时间 < 持续时间，则置为透明
             if (System.currentTimeMillis() - circle.mCreateTime < mDuration) {
                 mPaint.setAlpha(circle.getAlpha());
                 canvas.drawCircle(getWidth() / 2, getHeight() / 2, radius, mPaint);
@@ -108,6 +134,7 @@ public class WaveView extends View {
                 iterator.remove();
             }
         }
+
         if (mCircleList.size() > 0) {
             postInvalidateDelayed(10);
         }
@@ -130,8 +157,12 @@ public class WaveView extends View {
         mSpeed = speed;
     }
 
+    /**
+     * 产生圆圈
+     */
     private void newCircle() {
         long currentTime = System.currentTimeMillis();
+
         if (currentTime - mLastCreateTime < mSpeed) {
             return;
         }
@@ -142,18 +173,21 @@ public class WaveView extends View {
     }
 
     private class Circle {
+
         private long mCreateTime;
 
         Circle() {
             mCreateTime = System.currentTimeMillis();
         }
 
+        // 关于这里getAlpha、getCurrentRadius的方法计算不是那么的明白
         int getAlpha() {
             float percent = (getCurrentRadius() - mInitialRadius) / (mMaxRadius - mInitialRadius);
             return (int) (255 - mInterpolator.getInterpolation(percent) * 255);
         }
 
         float getCurrentRadius() {
+            // percent: 百分比
             float percent = (System.currentTimeMillis() - mCreateTime) * 1.0f / mDuration;
             return mInitialRadius + mInterpolator.getInterpolation(percent) * (mMaxRadius - mInitialRadius);
         }
